db9f3aeb9ca3c923fdb6c3a6e23b52157797835a
Csányi Gábor <a4shnl@inf.elte.hu>
2024-11-16 20:40:22 +0100

    Added plan duration log

--- src/MAPFPlanner.cpp
+++ src/MAPFPlanner.cpp
@@ -7,8 +7,15 @@ void MAPFPlanner::initialize(int preprocess_time_limit) {
 }
 
 void MAPFPlanner::plan(int time_limit, vector<Action>& actions) {
+    auto start {std::chrono::steady_clock::now()};
+
     auto limit {time_limit - std::chrono::duration_cast<milliseconds>(std::chrono::steady_clock::now()
          - env->plan_start_time).count() - DefaultPlanner::PLANNER_TIMELIMIT_TOLERANCE};
 
     pibt.nextStep(limit, actions);
+
+    auto end {std::chrono::steady_clock::now()};
+    auto duration {std::chrono::duration_cast<std::chrono::milliseconds>(end - start)};
+
+    std::cout << "******** Planning at timestep " << env->curr_timestep << " took " << duration.count() << "ms ********\n";
 }
